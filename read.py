0625c9cd0a48460b8c438817cef025ff1cd33e80
Larry Chen <z5155210@gmail.com>
2022-09-19 23:45:07 +0800

    update calc avg len

--- read.py
+++ read.py
@@ -8,3 +8,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1 # count = count + 1
 		if count % 1000 == 0: # count / 1000 的餘數 = 0
 			print(len(data))
+sum_len = 0			
+for d in data:
+	sum_len += len(d) # sum_len = sum_len + len(d)
+avg_len = sum_len / len(data)
+print('檔案已讀取完畢,總共有', len(data), '筆資料')
+print('平均長度為 ', avg_len)
