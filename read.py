3a2787b32f11a9ad4833b795cb731d16d69ce0c7
Larry Chen <z5155210@gmail.com>
2022-09-20 15:14:20 +0800

    add filtering function

--- read.py
+++ read.py
@@ -12,5 +12,16 @@ sum_len = 0
 for d in data:
 	sum_len += len(d) # sum_len = sum_len + len(d)
 avg_len = sum_len / len(data)
-print('檔案已讀取完畢,總共有', len(data), '筆資料')
-print('平均長度為 ', avg_len)
+print('檔案已讀取完畢,總共有', len(data), '筆留言')
+print('留言的平均長度為 ', avg_len)
+
+# 篩選留言
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('總共有', len(new), '筆留言長度小於100')
+
+print(data[1])
+print(len(data[1]))
